01399ae543c5c8e2a7f357606f523369b15b1446
Ramu-the-noob <adityasaji2006@gmail.com>
2025-07-22 20:28:22 +0530

    Create variables_comments.java

---
+++ variables_comments.java
@@ -0,0 +1,18 @@
+public class variables_comments {
+    public static void main(String[] args){
+        //variables and their types
+        int a = 0; //integers
+        float b = 0.0F; //floating point numbers
+        double c =0.0; //decimal numbers
+        char d = '0'; //character
+        String e = "string"; //string
+        
+        /*
+        this
+        is
+        a
+        multi-line
+        comment
+         */
+    }
+}
