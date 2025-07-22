46dcf87010a4483883005cb6bb9baba2e32f9a0d
Ramu-the-noob <adityasaji2006@gmail.com>
2025-07-22 21:26:42 +0530

    Create if_else_and_ternary.java

---
+++ if_else_and_ternary.java
@@ -0,0 +1,18 @@
+public class if_else_and_ternary {
+    public static void main(String[] args){
+        //if-else code blocks
+
+        boolean condition = true;
+        if(condition)
+        {
+            System.out.println("The condition is true");
+        }
+        else
+        {
+            System.out.println("The condition is false");
+        }
+
+        //ternary operators
+        System.out.println((5 > 3) ? "5 is greater than 3" : "5 is less than 3");
+    }
+}
